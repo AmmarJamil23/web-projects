8166f728b55a724da94ff7b41b22bdd9940f0630
Ammar Jamil <ammarjamil7000@gmail.com>
2025-08-12 13:51:01 +0500

    Add files via upload

---
+++ Weather.jsx
@@ -0,0 +1,15 @@
+const Weather = () => {
+    let temp = 18;
+    
+    if(temp < 15) {
+        return <h1>It is cold outside</h1>
+    }
+    else if(temp >= 15 && temp <=25){
+        return <h1>It is nice outside</h1>
+    }
+    else if(temp > 25){
+        return <h1> It is hot outside</h1>
+    }
+}
+
+export default Weather;
