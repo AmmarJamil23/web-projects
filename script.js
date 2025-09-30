cc78ca6ac6bb6f8383efae0dbaaa6fb58370acd3
Ammar Jamil <ammarjamil7000@gmail.com>
2025-09-30 09:49:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// alert("Hello Ammar");
+// document.write("Hello this is a sentence")
+// document.write("This is document 2 that is inline");
+
+// document.getElementById.innerHTML = "Hello World ";
+
+
+// if(document.getElementById.innerHTML == "Hello World"){
+//     console.log("It is matched");
+// }
+// else {
+//     console.log("It is not matched");
+// }
+
+// function func() {
+//     document.getElementById.innerHTML = "Hellllooo"
+// }
+
+// function validFunction() {
+//   const username = document.getElementById('a').value;
+//   const password = document.getElementById('b').value;
+
+//   if (username === "ammar" && password === "12345") {
+//     document.write("You are logged in")
+//   } else {
+//     document.write("Invalid credentials");
+//   }
+// }
+
+// function resetFunction() {
+//     document.getElementById('a').value = "";
+//     document.getElementById('b').value = "";
+
+// }
+// }
+
+
+// function colorchange() {
+//     document.querySelector('.textbox').style.border = "3px solid black";
+//     document.querySelector('.textbox').style.color = "blue";
+//     document.querySelector('.textbox').style.fontSize = "1.5rem";
+    
+// }
+
